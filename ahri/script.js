f02157c3c07d1cb6d10e63fea712212041c05f2f
Richard Sun <richardsun0713@gmail.com>
2014-01-14 14:46:20 -0800

    added some items (prototype), added ahri page

--- ahri/script.js
+++ ahri/script.js
@@ -9,15 +9,15 @@ function updateStats(champLevel) {
 	// Create itemList
 
 	// Calculate stats
-	var champHealth = 395 + 85 * (champLevel-1);
+	var champHealth = 380 + 80 * (champLevel-1);
 
-	var champHP5 = 5.75 + 0.5 * (champLevel-1);
+	var champHP5 = 5.5 + 0.6 * (champLevel-1);
 
-	var champMana = 0; // Champ uses health
+	var champMP = 250 + 50 * (champLevel-1);
 	
-	var champMP5 = 0; // Champ uses health
+	var champMP5 = 7 + 0.6 * (champLevel-1);
 	
-	var champAD = 55 + 3.2 * (champLevel-1);
+	var champAD = 50 + 3 * (champLevel-1);
 	var baseAD = champAD;
 	for (var i = 0; i < itemList.length; i++)
 	{
@@ -26,51 +26,66 @@ function updateStats(champLevel) {
 	}
 	var bonusAD = champAD - baseAD;
 	
-	var champAS = 0.651 * (1 + 0.03 * (champLevel-1));
+	var champAS = 0.668 * (1 + 0.02 * (champLevel-1));
 
 	var champAP = 0;
+	var baseAP = champAP;
+	for (var i = 0; i < itemList.length; i++)
+	{
+		if (itemList[i].hasOwnProperty("addAP"))
+			champAP = itemList[i].addAP(champAP);
+	}
+	var bonusAP = champAP - baseAP;
+	
+	var champArmor = 11 + 3.5 * (champLevel-1);
 
-	var champArmor = 14 + 3.8 * (champLevel-1);
-
-	var champMR = 30 + 1.25 * (champLevel-1);
+	var champMR = 30 + 0 * (champLevel-1);
+	
+	var champMS = 330;
 
 	// Update stats on page
-	$('#GenHealth').text(champHealth);
+	$('#HP').text(champHealth);
 	
 	champHP5 = champHP5.toFixed(2);
-	$('#GenHP5').text(champHP5);
+	$('#HP5').text(champHP5);
 	
-	$('#GenMana').text('N/A');
+	$('#MP').text(champMP);
 	
 	champMP5 = champMP5.toFixed(2);
-	$('#GenMP5').text('N/A');
+	$('#MP5').text(champMP5);
 	
 	champAD = champAD.toFixed(0);
 	bonusAD = bonusAD.toFixed(0);
-	$('#OffAD').text(champAD);
-	$('#OffADBonus').text('(+' + bonusAD + ')');
+	$('#AD').text(champAD);
+	$('#ADBonus').text('(+' + bonusAD + ')');
 	
 	champAS = champAS.toFixed(3);
-	$('#OffAS').text(champAS);
+	$('#AS').text(champAS);
 	
-	$('#OffAP').text('N/A');
+	$('#AP').text(champAP);
+	$('#APBonus').text('(+' + bonusAP + ')');
 	
 	champArmor = champArmor.toFixed(0);
-	$('#DefArmor').text(champArmor);
+	$('#Armor').text(champArmor);
 	
 	champMR = champMR.toFixed(0);
-	$('#DefMR').text(champMR);
+	$('#MR').text(champMR);
+	
+	$('#MoveSpeed').text(champMS);
 }
 
 function createItem(name)
 {
 	switch(name) {
+	// Amplifying Tome
 	case 'Amplifying Tome':
 		var amplifyingTome = new Object();
 		amplifyingTome.addAP = function (AP) {
 			return (AP + 20);
 		};
 		return amplifyingTome;
+		
+	// Ancient Coin
 	case 'Ancient Coin':
 		var ancientCoin = new Object();
 		ancientCoin.addHP5 = function (HP5) {
@@ -79,19 +94,24 @@ function createItem(name)
 		ancientCoin.addMP5 = function (MP5) {
 			return (MP5 + 3);
 		};
-		return amplifyingTome;
+		return ancientCoin;
+		
+	// B.F. Sword
 	case 'B.F. Sword':
 		var bfSword = new Object();
 		bfSword.addAD = function (AD) {
 			return (AD + 45);
 		};
 		return bfSword;
+		
+	// Long Sword
 	case 'Long Sword':
 		var longSword = new Object();
 		longSword.addAD = function (AD) {
 			return (AD + 20);
 		};
 		return longSword;
+		
 	default:
 		return 0;
 		}
